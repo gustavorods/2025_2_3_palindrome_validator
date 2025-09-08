8e1dc58082c9bed3cd075df3b3fe25400319ea0e
gustavorods <gustavo.rlsilva07@gmail.com>
2025-09-07 23:30:33 -0300

    Comments to help with reading

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,6 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        // get the user's response
         String userText = JOptionPane.showInputDialog("Type the word");
         boolean resultado = palindrome_validator(userText);
         
