5da312f01a7c0fa79ed2c23f200e3a4c0891531e
gustavorods <gustavo.rlsilva07@gmail.com>
2025-09-07 23:20:41 -0300

    refactor: changes the value of test functions

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,12 +9,12 @@ public class AppTest {
     // Test with real palindrome words
     @Test
     void palindrome_word() {
-        assertTrue(App.palindrome_validator("Ana"));
+        assertTrue(App.palindrome_validator("arara"));
     }
 
     // Test with not real palindrome words
     @Test
     void not_palindrome_word() {
-        assertFalse(App.palindrome_validator("Merda"));
+        assertFalse(App.palindrome_validator("Gustavo"));
     }
 }
